ce73c2b4d3410eb4e0b2e0047058a0376430c6e7
kayyenl <yenyenlee9@gmail.com>
2022-08-05 00:25:53 +0800

    finally implemented properly, the skeleton loading!!!

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -8,21 +8,76 @@ import {
 
 const Nav = () => {
     const [isLogin, setLogin] = useState(false)
+    const [load, setLoad] = useState(true)
     const [firstChar, setFirstChar] = useState("")
+    const [user, setUser] = useState("");
+
+    useEffect(() => { //is actually called many times when re-rendering happens, so its not exactly one time only
+        onAuthStateChanged(auth, (user) => {
+            setLoad(false)
+            console.log(user)
+          if (user) {
+            setUser(user)
+            setLogin(true)
+          }
+        })
+      }, [])
 
     function register() {
-        console.log('register')
+    console.log('register')
+    setLoad(true)
+    createUserWithEmailAndPassword(auth, 'bob@cheese.com', 'cheesewheel')
+        .then(({ user }) => {
+            console.log(user)
+            setLoad(false)
+        })
+        .catch((error) => {
+            console.log(error.message + 'yes')
+            setLoad(false)
+        })
     }
+
+    function login() {
+    console.log('login')
+    setLoad(true)
+    signInWithEmailAndPassword(auth, 'bob@cheese.com', 'cheesewheel')
+        .then(({ user }) => {
+            console.log(user)
+            setUser(user)
+            setLogin(true)
+            setLoad(false)
+        })
+        .catch((error) => {
+            console.log(error.message)
+        })
+    }
+
+    function logout() {
+        setLoad(true)
+        signOut(auth).then(() => {
+          console.log('signout success!')
+          setLoad(false)
+        }).catch(() => {
+          console.log('signout failure...')
+        })
+        setUser("")
+        setLogin(false)
+      }
+
     return (
         <div className="central">
-            {!isLogin ?
-                <div>
-                    <button onClick={() => register()}>Register</button> 
-                    <button onClick={() => login()}>Login</button> 
-                </div> : 
-                <div className="show__char">
-                    {firstChar} 
-                </div>
+            {
+                load ? 
+                <div className="loading__state"></div> : <>
+                {!isLogin ?
+                    <div>
+                        <button onClick={() => register()}>Register</button> 
+                        <button onClick={() => login()}>Login</button> 
+                    </div> : 
+                    <button className="show__char" onClick={() => logout()}>
+                        {user.email[0]} 
+                    </button>
+                } </>
             }
         </div>
     );
