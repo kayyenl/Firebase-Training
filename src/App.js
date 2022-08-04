7a933e5ce7f1527e2ce70404d1b6c005a17782d9
kayyenl <yenyenlee9@gmail.com>
2022-08-04 16:09:30 +0800

    trying out the login function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import { auth } from './firebase/init';
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword } from "firebase/auth";
 
 function App() {
   function register() {
     console.log('register')
+    createUserWithEmailAndPassword(auth, 'bob@cheese.com', 'cheesewheel')
+      .then((user) => {
+        console.log(user, 'yes')
+      })
+      .catch((error) => {
+        console.log(error, 'no')
+      })
+  }
+
+  function login() {
+    console.log('login')
+    signInWithEmailAndPassword(auth, 'bob@cheese.com', 'cheesewheel')
+      .then((user) => {
+        console.log(user, 'yes')
+      })
+      .catch((error) => {
+        console.log(error, 'no')
+      })
   }
   return (
     <div className="App">
       <button onClick={() => register()}>
         Register Now
+      </button>
+      <button onClick={() => login()}>
+        Login
       </button>
     </div>
   );
